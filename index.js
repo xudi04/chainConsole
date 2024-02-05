681222d286465e22857fb321172f8207a6567e07
xudi04 <129602928+xudi04@users.noreply.github.com>
2024-02-05 14:49:40 +0300

    send 1

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+app = express();
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+
+const proxyOptions = {
+    target: 'https://chaindnm1.onrender.com',
+    changeOrigin: true,
+  };
+
+
+  app.use('/blocks', createProxyMiddleware(proxyOptions));
+  app.use('/add', createProxyMiddleware({ ...proxyOptions, pathRewrite: { '^/add': '/add' }, method: 'POST' }));
+
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended : true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.listen(process.env.PORT,4000, (req,res) =>{
+    console.log("http://localhost:4000");
+});
